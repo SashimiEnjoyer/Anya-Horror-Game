33fbe5eaca0a4e65f6968469f4c0855c6fc3495b
RiceRollRoll <109127504+RiceRollRoll@users.noreply.github.com>
2023-11-30 15:53:46 +1300

    Level 1a Progress fix, Level 2 Door fix

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -7,11 +7,14 @@ public class Door : InteractableItems
     [Space]
     [SerializeField] GameObject objectToMove;
     public bool isSlide = false;
+    public bool isSlideAxisZ = false;
     public bool isReversed = false;
     public bool isPositive = false;
     public float deltaRotation = 90;
     public float deltaMove = 10;
     public float timeRotation = 1;
+    private bool isExecuting = false;
+    private float time = 0;
 
     private void Awake()
     {
@@ -19,45 +22,81 @@ public class Door : InteractableItems
             objectToMove = gameObject;
     }
 
+    void Update()
+    {
+        if (isExecuting)
+        {
+            time += Time.deltaTime;
+
+            if (time > timeRotation + 0.5)
+            {
+                isExecuting = false;
+                time = 0;
+            }
+        }
+    }
+
     public override void Execute()
     {
-        base.Execute();
-
-        if (isSlide)
+        if (!isExecuting)
         {
-            if (isReversed)
-            {
-                if (isPositive)
-                {
-                    //transform.Rotate(0, 90, 0);
-                    DOTweenModulePhysics.DOMoveX(objectToMove.GetComponent<Rigidbody>(), transform.position.x + deltaMove, 1f);
-                    isPositive = false;
+            base.Execute();
+            isExecuting = true;
 
-                }
-                else
+            if (isSlide)
+            {
+                if (isReversed)
                 {
-                    DOTweenModulePhysics.DOMoveX(objectToMove.GetComponent<Rigidbody>(), transform.position.x - deltaMove, 1f);
-                    isPositive = true;
+                    if (!isSlideAxisZ)
+                    {
+                        if (isPositive)
+                        {
+                            //transform.Rotate(0, 90, 0);
+                            DOTweenModulePhysics.DOMoveX(objectToMove.GetComponent<Rigidbody>(), transform.position.x + deltaMove, 1f);
+                            isPositive = false;
+
+                        }
+                        else
+                        {
+                            DOTweenModulePhysics.DOMoveX(objectToMove.GetComponent<Rigidbody>(), transform.position.x - deltaMove, 1f);
+                            isPositive = true;
+                        }
+                    }
+                    else
+                    {
+                        if (isPositive)
+                        {
+                            DOTweenModulePhysics.DOMoveZ(objectToMove.GetComponent<Rigidbody>(), transform.position.z + deltaMove, 1f);
+                            isPositive = false;
+
+                        }
+                        else
+                        {
+                            DOTweenModulePhysics.DOMoveZ(objectToMove.GetComponent<Rigidbody>(), transform.position.z - deltaMove, 1f);
+                            isPositive = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (isReversed)
+                {
+                    if (isPositive)
+                    {
+                        //transform.Rotate(0, 90, 0);
+                        DOTweenModulePhysics.DORotate(objectToMove.GetComponent<Rigidbody>(), transform.rotation.eulerAngles + new Vector3(0, deltaRotation, 0), timeRotation);
+                        isPositive = false;
+                    }
+                    else
+                    {
+                        DOTweenModulePhysics.DORotate(objectToMove.GetComponent<Rigidbody>(), transform.rotation.eulerAngles + new Vector3(0, -deltaRotation, 0), timeRotation);
+                        isPositive = true;
+                    }
                 }
             }
         }
-        else
-        {
-            if (isReversed)
-            {
-                if (isPositive)
-                {
-                    //transform.Rotate(0, 90, 0);
-                    DOTweenModulePhysics.DORotate(objectToMove.GetComponent<Rigidbody>(), transform.rotation.eulerAngles + new Vector3(0, deltaRotation, 0), timeRotation);
-                    isPositive = false;
-                }
-                else
-                {
-                    DOTweenModulePhysics.DORotate(objectToMove.GetComponent<Rigidbody>(), transform.rotation.eulerAngles + new Vector3(0, -deltaRotation, 0), timeRotation);
-                    isPositive = true;
-                }
-            }
-        }
+        
         
     }
 }
